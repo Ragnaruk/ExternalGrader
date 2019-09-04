29f96ec0d3393fd21facfce67efe9bbb7b4a6e4a
Ragnaruk <ima1365@me.com>
2019-09-05 02:06:34 +0300

    Added a function to clear working directory

--- external_grader/process_answer.py
+++ external_grader/process_answer.py
@@ -3,6 +3,10 @@ import logging
 import importlib
 
 from pika import channel, spec, BasicProperties
+import os
+import shutil
+
+from external_grader.config import HOME_PATH
 
 
 def grade_answer(current_channel: channel.Channel,
@@ -33,15 +37,18 @@ def grade_answer(current_channel: channel.Channel,
             }
         }
 
+        grade_script = importlib.import_module("grade_scripts."
+                                               + str(message["xqueue_body"]["grader_payload"])
+                                               + ".grade")
+
         try:
-            grade_script = importlib.import_module("grade_scripts."
-                                                   + str(message["xqueue_body"]["grader_payload"])
-                                                   + ".grade")
             if "xqueue_files" in message:
                 score, msg = grade_script.main(message["xqueue_body"]["student_response"],
                                                message["xqueue_files"])
             else:
                 score, msg = grade_script.main(message["xqueue_body"]["student_response"])
+
+            clear_working_directory()
 
             response["xqueue_body"]["correct"] = True
             response["xqueue_body"]["score"] = score if score else 0
@@ -53,7 +60,7 @@ def grade_answer(current_channel: channel.Channel,
 
         logging.getLogger("ExternalGrader").info("Response: %s", response)
 
-        # Send a response and acknowledge message in queue
+        # Send a response
         current_channel.basic_publish(
             exchange="",
             routing_key=properties.reply_to,
@@ -62,4 +69,16 @@ def grade_answer(current_channel: channel.Channel,
     except Exception as exception:
         logging.getLogger("ExternalGrader").error(exception)
 
+    # Acknowledge message in queue
     current_channel.basic_ack(delivery_tag=basic_deliver.delivery_tag)
+
+
+def clear_working_directory():
+    """
+    Clears the working directory.
+    """
+    for root, dirs, files in os.walk(HOME_PATH):
+        for f in files:
+            os.unlink(os.path.join(root, f))
+        for d in dirs:
+            shutil.rmtree(os.path.join(root, d))
