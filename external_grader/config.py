b34628f17c36a60fb9c81c6ae169d159455ab1b4
Ragnaruk <ima1365@me.com>
2019-10-09 12:42:01 +0300

    Change default host to fit docker-compose

--- external_grader/config.py
+++ external_grader/config.py
@@ -4,7 +4,7 @@ HOME_PATH = Path("/home/")
 VERIFICATION_FILES_PATH = Path("/root/")
 LOG_FILE_PATH = Path(__file__).parent.parent / "grader.log"
 
-RABBITMQ_HOST = "localhost"
+RABBITMQ_HOST = "rabbitmq"
 RABBITMQ_PORT = 5672
 RABBITMQ_CONSUMPTION_QUEUE = "student_answers"
 RABBITMQ_CALLBACK_QUEUE = "student_grades"
