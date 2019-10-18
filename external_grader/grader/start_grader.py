58077fe45429d43c486e1ef08b938e6f1dc347d7
Ragnaruk <ima1365@me.com>
2019-10-18 18:51:24 +0300

    Improved message broker choosing process

--- external_grader/grader/start_grader.py
+++ external_grader/grader/start_grader.py
@@ -2,7 +2,7 @@ from time import sleep
 
 import pika.exceptions
 
-from external_grader.config.config import CONNECTION_RETRY_TIME
+from external_grader.config.config import CONNECTION_RETRY_TIME, MESSAGE_BROKER
 from external_grader.grader.logs import get_logger
 from external_grader.grader.message_brokers.rabbitmq import receive_messages
 from external_grader.utils.decorators import log_exceptions
@@ -26,13 +26,23 @@ def listen_to_broker():
     logger = get_logger("start_grader")
 
     try:
-        receive_messages()
-    except pika.exceptions.AMQPConnectionError:
-        logger.error("Failed to connect to RabbitMQ broker.")
-    except pika.exceptions:
-        logger.error("Failed to connect to RabbitMQ broker.")
-    except KeyboardInterrupt:
-        logger.info("Program has been stopped manually.")
+        if MESSAGE_BROKER.TYPE == "rabbitmq":
+            try:
+                receive_messages(
+                    MESSAGE_BROKER.HOST,
+                    MESSAGE_BROKER.PORT,
+                    MESSAGE_BROKER.USER,
+                    MESSAGE_BROKER.PASS,
+                    MESSAGE_BROKER.QUEUE,
+                )
+            except pika.exceptions.AMQPConnectionError:
+                logger.error("Failed to connect to RabbitMQ broker.")
+            except pika.exceptions as exception:
+                logger.error("Unhandled pika error: %s.", exception, exc_info=True)
+            except KeyboardInterrupt:
+                logger.info("Program has been stopped manually.")
+    except AttributeError as exception:
+        logger.error(exception, exc_info=True)
 
 
 if __name__ == "__main__":
