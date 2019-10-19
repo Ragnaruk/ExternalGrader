5214a57ea08ff8bce6147c1cef63c45503cb49be
Ragnaruk <ima1365@me.com>
2019-10-19 20:03:24 +0300

    Updated paths Added epicbox settings

--- external_grader/config/config.py
+++ external_grader/config/config.py
@@ -3,11 +3,10 @@ General config.
 """
 from logging import DEBUG, INFO, ERROR
 from pathlib import Path
-import importlib
-from os import getenv
 
-PATH_EXECUTION_DIRECTORY = Path("/home/")
 PATH_LOG_DIRECTORY = Path.cwd() / "logs"
+PATH_DATA_DIRECTORY = Path.cwd() / "data"
+PATH_GRADER_SCRIPTS_DIRECTORY = Path.cwd() / "external_grader" / "grader_scripts"
 
 # Possible log levels: DEBUG, INFO, ERROR
 LOG_LEVEL = DEBUG
@@ -16,6 +15,25 @@ LOG_FORMAT = "%(asctime)s - %(levelname)-5s - %(filename)s:%(lineno)d - %(messag
 # Number of seconds to wait between connections attempts to message broker
 CONNECTION_RETRY_TIME = 10
 
-# Name of the python file with message broker config
-# Used when
+# Name of the python file with default message broker config
 QUEUE_CONFIG_NAME = "rabbitmq_example"
+
+# https://github.com/StepicOrg/epicbox
+EPICBOX_SETTINGS = {
+    "container_limits": {
+        # CPU time in seconds, None for unlimited
+        "cputime": 30,
+        # Real time in seconds, None for unlimited
+        "realtime": 150,
+        # Memory in megabytes, None for unlimited
+        "memory": 512,
+        # Limit the max processes the sandbox can have
+        # -1 or None for unlimited(default)
+        "processes": -1
+    },
+    "profile": {
+        "docker_image": "python:3.8-alpine",
+        "read_only": False
+    }
+}
+
