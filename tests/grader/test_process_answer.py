5787e1961636ea067f7e7402130e9392141cb5e5
Ragnaruk <ima1365@me.com>
2019-10-19 22:54:29 +0300

    Updated tests

--- tests/grader/test_process_answer.py
+++ tests/grader/test_process_answer.py
@@ -18,9 +18,8 @@ def test_process_answer_correct():
     }
 
     expected_response: dict = {
-        "correct": True,
-        "score": 100,
-        "msg": "Верный ответ."
+        "score": '100',
+        "msg": "Верный ответ.\n"
     }
 
     assert process_answer.process_answer(answer) == expected_response
@@ -38,24 +37,34 @@ def test_process_answer_incorrect():
     }
 
     expected_response: dict = {
-        "correct": False,
-        "score": 0,
-        "msg": "Ответ не равен 5."
+        "score": '50',
+        "msg": "Ответ не равен 5.\n"
     }
 
-    assert process_answer.process_answer(answer) != expected_response
+    assert process_answer.process_answer(answer) == expected_response
 
 
-def test_process_answer_module_not_found():
-    """
-    Test grader.process_answer.process_answer function.
-    """
-    answer: dict = {
-        "xqueue_body": {
-            "student_response": "response",
-            "grader_payload": "qwerty123456"
-        }
-    }
+# def test_process_answer_module_not_found():
+#     """
+#     Test grader.process_answer.process_answer function.
+#     """
+#     answer: dict = {
+#         "xqueue_body": {
+#             "student_response": "response",
+#             "grader_payload": "qwerty123456"
+#         }
+#     }
+#
+#     with pytest.raises(ModuleNotFoundError):
+#         process_answer.process_answer(answer)
 
-    with pytest.raises(ModuleNotFoundError):
-        process_answer.process_answer(answer)
+
+# def test_process_answer():
+#     answer: dict = {
+#         "xqueue_body": {
+#             "student_response": "5",
+#             "grader_payload": "1"
+#         }
+#     }
+#
+#     print(process_answer.process_answer(answer))
