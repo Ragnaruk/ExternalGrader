180f810e2209283a179f7da281f0dfbb752b929f
Ragnaruk <ima1365@me.com>
2019-10-19 20:03:34 +0300

    Added exceptions

---
+++ external_grader/grader/exceptions.py
@@ -0,0 +1,10 @@
+"""
+Custom exceptions.
+"""
+
+
+class FailedFilesLoadException(Exception):
+    """
+    Exception which is called when grader failed to load files required for grading.
+    """
+    pass
