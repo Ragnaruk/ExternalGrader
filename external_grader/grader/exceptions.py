c1a299c148bfe8a4c83dada0d88410684df43dcf
Ragnaruk <ima1365@me.com>
2019-10-19 22:51:34 +0300

    Added custom exceptions

--- external_grader/grader/exceptions.py
+++ external_grader/grader/exceptions.py
@@ -8,3 +8,16 @@ class FailedFilesLoadException(Exception):
     Exception which is called when grader failed to load files required for grading.
     """
     pass
+
+
+class InvalidSubmissionException(Exception):
+    """
+    Exception which is called when either xqueue_body or grader_payload fields don't exist
+    """
+
+
+class InvalidResponseException(Exception):
+    """
+    Exception which is called when either student asnwer doesn't exist or is empty.
+    """
+    pass
