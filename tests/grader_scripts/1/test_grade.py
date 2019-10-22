d94ac0410b87d0a217db2950d3080e2039eb3bef
Ragnaruk <ima1365@me.com>
2019-10-22 20:01:11 +0300

    Moved grader script tests to a different file and added several more

---
+++ tests/grader_scripts/1/test_grade.py
@@ -0,0 +1,82 @@
+"""
+PyTest test file for grader_scripts.1.grade module.
+"""
+import pytest
+
+from external_grader.grader import process_answer
+from external_grader.grader.exceptions import InvalidGraderScriptException
+
+
+def test_grade_incorrect_not_number():
+    """
+    Test grader_scripts.1.grade script.
+    """
+    answer: dict = {
+        "xqueue_body": {
+            "student_response": "hello",
+            "grader_payload": "1"
+        }
+    }
+
+    expected_response: dict = {
+        "correct": False,
+        "score": 0,
+        "msg": "Ответ — не число.\n"
+    }
+
+    assert process_answer.process_answer(answer) == expected_response
+
+
+def test_grade_incorrect_different_number():
+    """
+    Test grader_scripts.1.grade script.
+    """
+    answer: dict = {
+        "xqueue_body": {
+            "student_response": "4",
+            "grader_payload": "1"
+        }
+    }
+
+    expected_response: dict = {
+        "correct": False,
+        "score": 50,
+        "msg": "Ответ не равен 5.\n"
+    }
+
+    assert process_answer.process_answer(answer) == expected_response
+
+
+def test_grade_incorrect_invalid_output():
+    """
+    Test grader_scripts.1.grade script.
+    """
+    answer: dict = {
+        "xqueue_body": {
+            "student_response": "-1000",
+            "grader_payload": "1"
+        }
+    }
+
+    with pytest.raises(InvalidGraderScriptException):
+        assert process_answer.process_answer(answer)
+
+
+def test_grade_correct():
+    """
+    Test grader_scripts.1.grade script.
+    """
+    answer: dict = {
+        "xqueue_body": {
+            "student_response": "5",
+            "grader_payload": "1"
+        }
+    }
+
+    expected_response: dict = {
+        "correct": True,
+        "score": 100,
+        "msg": "Верный ответ.\n"
+    }
+
+    assert process_answer.process_answer(answer) == expected_response
