ca2ed2217dae5d6a994aa685918115123873a879
Ragnaruk <ima1365@me.com>
2019-10-22 22:27:52 +0300

    Added more tests

--- tests/external_grader/grader/test_process_answer.py
+++ tests/external_grader/grader/test_process_answer.py
@@ -6,6 +6,8 @@ import pytest
 from external_grader.grader import process_answer
 from external_grader.grader.exceptions import FailedFilesLoadException, \
     InvalidSubmissionException, InvalidGraderScriptException
+from external_grader.config.config import PATH_DATA_DIRECTORY, PATH_GRADER_SCRIPTS_DIRECTORY, \
+    EPICBOX_SETTINGS
 
 
 def test_submission_validate_valid_body():
@@ -151,7 +153,7 @@ def test_submission_get_response_body():
         }
     }
 
-    expected_respone: str = '5'
+    expected_respone: str = "5"
 
     assert process_answer.submission_get_response(submission) == expected_respone
 
@@ -222,3 +224,159 @@ def test_submission_settings_load_invalid():
 
     with pytest.raises(InvalidGraderScriptException):
         assert process_answer.settings_load(script_name)
+
+
+def test_submission_settings_parse_valid_empty():
+    """
+    Test grader.process_answer.settings_parse function.
+    """
+    script_name: str = "1"
+    settings: dict = {}
+
+    expected_response: tuple = (
+        [],
+        EPICBOX_SETTINGS["profile"],
+        EPICBOX_SETTINGS["container_limits"]
+    )
+
+    assert process_answer.settings_parse(script_name, settings) == expected_response
+
+
+def test_submission_settings_parse_valid_container_limits():
+    """
+    Test grader.process_answer.settings_parse function.
+    """
+    script_name: str = "1"
+    settings: dict = {
+        "container_limits": {
+            "cputime": 1,
+            "realtime": 3,
+            "memory": 64,
+            "processes": -1
+        }
+    }
+
+    expected_response: tuple = (
+        [],
+        EPICBOX_SETTINGS["profile"],
+        settings["container_limits"]
+    )
+
+    assert process_answer.settings_parse(script_name, settings) == expected_response
+
+
+def test_submission_settings_parse_valid_profile():
+    """
+    Test grader.process_answer.settings_parse function.
+    """
+    script_name: str = "1"
+    settings: dict = {
+        "profile": {
+            "docker_image": "ragnaruk/python:latest",
+            "user": "guest",
+            "read_only": False,
+            "network_disabled": True
+        }
+    }
+
+    expected_response: tuple = (
+        [],
+        settings["profile"],
+        EPICBOX_SETTINGS["container_limits"]
+    )
+
+    assert process_answer.settings_parse(script_name, settings) == expected_response
+
+
+def test_submission_settings_parse_valid_all():
+    """
+    Test grader.process_answer.settings_parse function.
+    """
+    script_name: str = "1"
+    settings: dict = {
+        "files": {
+            "external": [
+                {
+                    "name": "test-external.txt",
+                    "link": "http://captive.apple.com"
+                }
+            ],
+            "local": [
+                {
+                    "name": "test-local.txt",
+                    "path": "settings.json"
+                }
+            ]
+        },
+        "container_limits": {
+            "cputime": 1,
+            "realtime": 3,
+            "memory": 64,
+            "processes": -1
+        },
+        "profile": {
+            "docker_image": "ragnaruk/python:latest",
+            "user": "guest",
+            "read_only": False,
+            "network_disabled": True
+        }
+    }
+
+    expected_response: tuple = (
+        [
+            {
+                "type": "external",
+                "name": "test-external.txt",
+                "path": PATH_DATA_DIRECTORY / "grader_scripts/1/test-external.txt"
+            },
+            {
+                "type": "local",
+                "name": "test-local.txt",
+                "path": PATH_GRADER_SCRIPTS_DIRECTORY / "1/settings.json"
+            }
+        ],
+        settings["profile"],
+        settings["container_limits"]
+    )
+
+    assert process_answer.settings_parse(script_name, settings) == expected_response
+
+
+def test_submission_settings_parse_invalid_external():
+    """
+    Test grader.process_answer.settings_parse function.
+    """
+    script_name: str = "1"
+    settings: dict = {
+        "files": {
+            "external": [
+                {
+                    "name": "abcdef.txt",
+                    "link": "http://abc.def"
+                }
+            ]
+        }
+    }
+
+    with pytest.raises(FailedFilesLoadException):
+        process_answer.settings_parse(script_name, settings)
+
+
+def test_submission_settings_parse_invalid_local():
+    """
+    Test grader.process_answer.settings_parse function.
+    """
+    script_name: str = "1"
+    settings: dict = {
+        "files": {
+            "local": [
+                {
+                    "name": "abcdef.txt",
+                    "path": "abc.def"
+                }
+            ]
+        }
+    }
+
+    with pytest.raises(FailedFilesLoadException):
+        process_answer.settings_parse(script_name, settings)
